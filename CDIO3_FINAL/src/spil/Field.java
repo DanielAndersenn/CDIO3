2063d60eb48711f5e0e2a74efc46ab9b28c9fb8d
Daniel Andersen <dsa-1992@hotmail.com>
2015-11-25 22:52:23 +0100

    Hej

--- CDIO3_FINAL/src/spil/Field.java
+++ CDIO3_FINAL/src/spil/Field.java
@@ -14,9 +14,9 @@ public class Field {
 		
 		
 		//@ Christian: Det er her vi er i tvivl om hvordan vi får den information fra feltet hvorpå kaldet er sket. Se Game.java linje 102
-		if(player.getBoardPos() == ) {
+		//if(player.getBoardPos() == ) {
 			
-		}
+		//}
 		
 	}
 	
