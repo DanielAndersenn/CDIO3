8a14c2fb91776f731c13280f30f5b9fb88a1dcdd
Daniel Andersen <dsa-1992@hotmail.com>
2015-11-25 22:46:58 +0100

    Whatever

---
+++ CDIO3_FINAL/src/spil/Field.java
@@ -0,0 +1,23 @@
+package spil;
+
+public class Field {
+
+	String name;
+	
+	public Field() {
+	}
+	
+	public void landOnField(Player player) {
+		System.out.println(player.getUserName() + " landed on: " + name + ".");
+		int banTest;
+		boolean bInput = true;
+		
+		
+		//@ Christian: Det er her vi er i tvivl om hvordan vi får den information fra feltet hvorpå kaldet er sket. Se Game.java linje 102
+		if(player.getBoardPos() == ) {
+			
+		}
+		
+	}
+	
+}
